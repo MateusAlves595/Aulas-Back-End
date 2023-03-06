3283387df4523d56f1814764e99e87539637cb5c
MATEUS ALVES DA SILVA <22282205@CFP127ALUNO.LOCAL>
2023-03-06 16:10:54 -0300

    funções finalizadas

--- Aula 07/functions.js
+++ Aula 07/functions.js
@@ -6,12 +6,12 @@
  ************************************************************************/
 var listaGeral = require('./estados_cidades.js')
 
-const getListaDeEstados = function() {
+const getListaDeEstados = function () {
 
     let novoArray = []
     let novoJson = {}
 
-    listaGeral.estadosCidades.estados.forEach(function(itemEstado){
+    listaGeral.estadosCidades.estados.forEach(function (itemEstado) {
         novoArray.push(itemEstado.sigla)
     })
 
@@ -21,4 +21,137 @@ const getListaDeEstados = function() {
     return novoJson
 }
 
-getListaDeEstados()
+const getDadosEstado = function (siglaEstado) {
+    let novoJson = {}
+    let uf = siglaEstado
+    let nome
+    let capital
+    let regiao
+
+    listaGeral.estadosCidades.estados.forEach(function (dadosEstado) {
+        uf = dadosEstado.sigla
+        nome = dadosEstado.nome
+        capital = dadosEstado.capital
+        regiao = dadosEstado.regiao
+
+        if (uf == siglaEstado) {
+            novoJson.uf = uf
+            novoJson.descricao = nome
+            novoJson.capital = capital
+            novoJson.regiao = regiao
+        }
+    })
+
+    if (novoJson == undefined) {
+        return false
+    } else if (novoJson == '') {
+        return false
+    } else{
+        return novoJson
+    }
+
+}
+
+const getCapitalEstado = function (siglaEstado) {
+    let novoJson = {}
+    let uf = siglaEstado
+    let nome
+    let capital
+
+    listaGeral.estadosCidades.estados.forEach(function (dadosEstado) {
+        uf = dadosEstado.sigla
+        nome = dadosEstado.nome
+        capital = dadosEstado.capital
+
+        if (uf == siglaEstado) {
+            novoJson.uf = uf
+            novoJson.descricao = nome
+            novoJson.capital = capital
+        }
+    })
+
+    if (novoJson == undefined) {
+        return false
+    } else if (novoJson == '') {
+        return false
+    } else{
+        return novoJson
+    }
+}
+
+const getEstadosRegiao = function (regiaoEstado) {
+    let regiao = regiaoEstado.toUpperCase()
+    let estados = []
+    let novoJson = {}
+
+    const estadosRegiao = listaGeral.estadosCidades.estados.filter(function (estadosDados) {
+        return estadosDados.regiao == regiao
+    })
+
+    estados = estadosRegiao.map(function (estadosDados) {
+        return {
+            uf: estadosDados.sigla,
+            descricao: estadosDados.nome
+        }
+    })
+
+    novoJson.regiao = regiaoEstado
+    novoJson.estados = estados
+
+    if (novoJson == undefined) {
+        return false
+    } else if (novoJson == '') {
+        return false
+    } else
+        return novoJson
+    }
+
+const getCapitalPais = function () {
+    let capitalJSON = {}
+    let capitaisBrasil =
+        listaGeral.estadosCidades.estados.filter(estadosDados => estadosDados.capital_pais != undefined)
+            .map(estadosDados => ({
+                capital_atual: estadosDados.capital_pais.capital,
+                uf: estadosDados.sigla,
+                descricao: estadosDados.nome,
+                capital: estadosDados.capital,
+                regiao: estadosDados.regiao,
+                capital_pais_ano_inicio: estadosDados.capital_pais.ano_inicio,
+                capital_pais_ano_termino: estadosDados.capital_pais.ano_fim
+            }))
+
+    capitalJSON.capitaisBrasil = capitaisBrasil
+
+    return capitaisBrasil
+}
+
+const getCidades = function (siglaEstado) {
+    let uf = siglaEstado.toUpperCase()
+    let nomeEstado
+    let listCidadesJSON = {}
+    let cidades = []
+
+    listaGeral.estadosCidades.estados.forEach(function (estadosDados) {
+        if (estadosDados.sigla == uf) {
+            nomeEstado = estadosDados.nome
+
+            estadosDados.cidades.forEach(function (cidadeDados) {
+                cidades.push(cidadeDados.nome)
+            })
+        }
+    })
+
+    listCidadesJSON.uf = uf
+    listCidadesJSON.descricao = nomeEstado
+    listCidadesJSON.quantidade_cidades = cidades.length
+    listCidadesJSON.cidades = cidades
+
+    if (listCidadesJSON == undefined) {
+        return false
+    } else if (listCidadesJSON == '') {
+        return false
+    } else {
+        return listCidadesJSON
+    }
+
+}
